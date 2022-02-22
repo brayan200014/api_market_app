9231deae43fc3ab7ab8c58510243ee353214fdbf
Josue Hernandez <josue200014@outlook.com>
2022-02-21 22:55:19 -0600

    Adicion de Paquetes y Carpetas

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+require('dotenv').config(); 
+
+const app= express(); 
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json()); 
+
+app.listen(6001, ()=> {
+    console.log("el servidor escucha el puerto 6001");
+});
+
