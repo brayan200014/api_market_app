9b7d0622eddf31381ffaca29da7cd94017ad18d6
IkerOrt2001 <ikerortezcolindres@gmail.com>
2022-02-26 00:57:22 -0600

    Modelo Cliente

---
+++ src/modelos/modeloClientes.js
@@ -0,0 +1,53 @@
+const sequelize = require('sequelize');
+const db = require('../configuraciones/db');
+const bcrypt = require('bcrypt');
+
+const Cliente = db.define("cliente",
+{
+    id:{
+        type: sequelize.INTEGER,
+        primaryKey: true ,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    Nombre:{
+        type: sequelize.STRING(45),
+        allowNull: true,
+    },
+    correo:{
+        type: sequelize.STRING(250),
+        allowNull: false,
+    },
+    contraseña:{
+        type: sequelize.STRING(250),
+        allowNull: false,
+    },
+    creacion:{
+        type: sequelize.DATE,
+        allowNull: false,
+    },
+    estado:{
+        type: sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: true,
+    },
+},
+{
+    tableName: "clientes",
+    timestamps: false,
+    hooks:{
+        beforeCreate(cliente){
+            const hast = bcrypt.hashSync(cliente.contraseña, 10);
+            cliente.contraseña=hast;
+        },
+        beforeUpdate(cliente){
+            const hast = bcrypt.hashSync(cliente.contraseña, 10);
+            cliente.contraseña=hast;
+        }
+    }
+}
+);
+Cliente.prototype.VerificarContraseña=(con, com) => {
+    return bcrypt.compareSync(con, com);
+}
+module.exports=Cliente;
