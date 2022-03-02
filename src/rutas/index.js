fe254707d8c1b47395d6ec9f87f3cfa3db7dc633
jete20 <78774120+jete20@users.noreply.github.com>
2022-03-01 21:23:01 -0600

    ultimo

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -5,6 +5,4 @@ router.get('/', controladorInicio.inicio);
 router.get('/empleados',controladorInicio.empleados);
 router.get('/sucursales',controladorInicio.sucursales);
 router.get('/ciudades',controladorInicio.ciudades);
-router.get('/categorias',controladorInicio.categorias);
-router.get('/inventario',controladorInicio.inventario);
 module.exports=router;
