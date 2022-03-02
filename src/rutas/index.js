9be1899c5db62c4fa6aced61d9ba13824a3d58db
jete20 <78774120+jete20@users.noreply.github.com>
2022-03-01 21:20:09 -0600

    Ultimo Merge

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const controladorInicio = require('../controladores/controladorInicio');
 const router = Router();
+router.get('/', controladorInicio.inicio);
+router.get('/empleados',controladorInicio.empleados);
+router.get('/sucursales',controladorInicio.sucursales);
+router.get('/ciudades',controladorInicio.ciudades);
 router.get('/categorias',controladorInicio.categorias);
 router.get('/inventario',controladorInicio.inventario);
 module.exports=router;
-
